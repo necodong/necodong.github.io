72d194bf300b3c3dee4ae36406c7ea9d1a8e8968
x8128 <aliasbonfire@gmail.com>
2022-07-03 01:17:29 +0900

    initial nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Intro } from "../components";
+
+const Home: NextPage = () => {
+  return (
+    <article>
+      <Intro />
+      <div>Necodong</div>
+    </article>
+  );
+};
+
+export default Home;
