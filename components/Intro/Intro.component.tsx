1b2ab3e4799ce0a5f02def5b03ff67c45af9cd9f
x8128 <aliasbonfire@gmail.com>
2022-07-18 22:05:25 +0900

    necodong loading

--- components/Intro/Intro.component.tsx
+++ components/Intro/Intro.component.tsx
@@ -1,25 +1,20 @@
 import type { NextComponentType } from "next";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 import IntroStyle from "./Intro.module.scss";
 
 const Intro: NextComponentType = () => {
   const introRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const introElement = introRef.current;
-    if (!introElement) return;
-
-    setTimeout(() => introElement.remove(), 1500);
-  }, []);
-
   return (
     <section ref={introRef} className={IntroStyle.container}>
+
       <div className={IntroStyle.logoWrapper}>
         <Image src="/neco.svg" alt="Necodong Logo" layout="fill" />
       </div>
-    </section>
+      <div className={IntroStyle.logoBackground} />
+    </section >
   );
 };
 
